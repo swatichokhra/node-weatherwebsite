4f789523b29d7a7003bb0c73bf172a1161b443f2
unknown <chokhra.swati@gmail.com>
2019-06-13 15:12:09 +0530

    init commit

---
+++ src/app.js
@@ -0,0 +1,129 @@
+const path = require('path');  //core node module
+const express = require('express');
+const hbs = require('hbs');
+const geocode = require('./utils/geocode.js');
+const forecast= require('./utils/forecast.js');
+
+//console.log(__dirname);
+//console.log(__filename);
+//console.log(path.join(__dirname,'../public'));
+
+const app = express();
+
+//Define paths for express config
+const publicDirectoryPath = path.join(__dirname,'../public');
+const viewsPath = path.join(__dirname,'../templates/views');
+const partialsPath = path.join(__dirname,'../templates/partials');
+
+//setup handlebars engine and view location
+app.set('view engine','hbs');
+app.set('views',viewsPath);
+hbs.registerPartials(partialsPath);
+
+//setup static directory to serve
+app.use(express.static(publicDirectoryPath));
+
+
+app.get('/index',(req,res)=>{
+
+    res.render('index',{
+        title: 'Weather App',
+        name : 'Andrew Mead'
+    });
+});
+//app.com
+//app.com/help
+//app.com/about
+
+app.get('',(req,res)=>{
+    // res.send('Hello Express');
+    res.render('index',{
+        title:'Weather',
+        name : 'Andrew Mead'
+    });
+});
+
+app.get('/help',(req,res)=> {
+    res.render('help', {
+        title : 'Help',
+        helpText: 'This is helpful text',
+        name : 'Andrew Mead'
+    });
+});
+
+
+
+app.get('/about',(req,res)=>{
+    res.render('about', {
+        title: 'About Me',
+        name: 'Andrew Mead'
+    })
+});
+
+
+
+app.get('/weather',(req,res)=> {
+    if (!req.query.address) {
+        return res.send({
+            error: 'you must provide an address'
+        });
+    }
+    // console.log(req.query);
+    // console.log(req.query.address);
+    geocode(req.query.address, (error, {latitude, longitude, location} = {}) => {
+        if (error) {
+            return res.send({
+                error
+            });
+        }
+        forecast(latitude, longitude, (error, forecastData) => {
+            if (error) {
+                return res.send({
+                    error
+                });
+            }
+            res.send({
+                forecast: forecastData,
+                location,
+                address: req.query.address
+            });
+            // res.send({
+            //     forecast : '50 degress',
+            //     location : 'Philadelphia',
+            //     address : req.query.address
+        });
+    })
+});
+
+app.get('/products', (req, res) => {
+    if (!req.query.search) {
+        return res.send({
+            error: 'you must provide search term'
+        })
+    }
+    console.log(req.query);
+    console.log(req.query.search);
+    res.send({
+        products: []
+    });
+});
+
+app.get('/help/*', (req, res) => {
+    res.render('404', {
+        title: '404',
+        name: 'Andrew Mead',
+        errorMessage: 'Help Article not found'
+    });
+});
+
+app.get('*', (req, res) => {
+    res.render('404', {
+        title: '404',
+        name: 'Andrew Mead',
+        errorMessage: 'Page not found'
+    });
+});
+
+app.listen(3000, () => {
+    console.log('server is up on port 3000.')
+});
---
+++ src/utils/forecast.js
@@ -0,0 +1,30 @@
+//
+// Goal: Create a reusable function for getting the forecast
+//
+// 1. Setup the "forecast" function in utils/forecast.js
+// 2. Require the function in app.js and call it as shown below
+// 3. The forecast function should have three potential calls to callback:
+//    - Low level error, pass string for error
+//    - Coordinate error, pass string for error
+//    - Success, pass forecast string for data (same format as from before)
+
+const request = require('request');
+const forecast = (lat,long,callback) => {
+
+    const url = 'https://api.darksky.net/forecast/af892970f49166a3d9a7e852425a0899/' + lat + ',' + long;
+    request({ url, json : true}, (error,response) => {
+        if (error)
+        {
+            callback('Unable to connect to weather services',undefined);
+        }
+        else if (response.body.error ){
+            callback('Unable to find coordinate.try another search',undefined);
+        }
+        else {
+            callback(undefined, 'It is currently ' + response.body.currently.temperature + ' degrees out. There is a ' + response.body.currently.precipProbability + '% chance of rain.');
+        }
+
+    })
+}
+
+module.exports = forecast;
