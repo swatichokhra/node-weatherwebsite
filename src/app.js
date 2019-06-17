cfb874079c4b8fe28a0c8c870d2c3b5cb250b8a0
unknown <chokhra.swati@gmail.com>
2019-06-17 08:10:49 +0530

    setup for heroku

--- src/app.js
+++ src/app.js
@@ -14,7 +14,7 @@ const app = express();
 const publicDirectoryPath = path.join(__dirname,'../public');
 const viewsPath = path.join(__dirname,'../templates/views');
 const partialsPath = path.join(__dirname,'../templates/partials');
-
+const port = process.env.PORT || 3000;
 //setup handlebars engine and view location
 app.set('view engine','hbs');
 app.set('views',viewsPath);
@@ -124,6 +124,6 @@ app.get('*', (req, res) => {
     });
 });
 
-app.listen(3000, () => {
-    console.log('server is up on port 3000.')
+app.listen(port, () => {
+    console.log('server is up on port ' + port);
 });
